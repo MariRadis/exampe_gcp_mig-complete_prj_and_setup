7e63a508ff7fc12150f787538ee53721cce8b1fe
Maryam <20474103+MaryamBerg@users.noreply.github.com>
2025-06-22 18:56:49 +0200

    .

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,3 +1,8 @@
+import {
+  id = var.project_id
+  to = google_project.project
+}
+
 resource "google_project" "project" {
   name       = var.project_name
   project_id = var.project_id
@@ -30,7 +35,8 @@ resource "google_project_iam_member" "terraform_sa_roles" {
     "roles/monitoring.viewer",
     "roles/iam.serviceAccountUser",
     "roles/logging.logWriter",
-    "roles/resourcemanager.projectCreator"
+    "roles/storage.admin"
+    # "roles/resourcemanager.projectCreator" becasue I do not have org I do not have org admin
   ])
 
 
@@ -40,7 +46,18 @@ resource "google_project_iam_member" "terraform_sa_roles" {
   member  = "serviceAccount:${google_service_account.terraform.email}"
 }
 
+resource "google_storage_bucket" "terraform_state" {
+  name     = "tf-project-states"
+  project  = var.project_id
+  location = "EUROPE-WEST1"
+  force_destroy = true
 
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+}
 #Responsibilities:
 #Create GCP project
 #
@@ -53,5 +70,7 @@ resource "google_project_iam_member" "terraform_sa_roles" {
 #roles/compute.networkAdmin	Manage networks, subnets, and firewalls
 #roles/iam.serviceAccountUser	Attach service accounts (like vm_sa) to VMs and templates
 #roles/monitoring.viewer	Needed if autoscaler or instance health policies inspect metrics
+#
+#Create state bucket
 #
 #Output service account email & project ID
