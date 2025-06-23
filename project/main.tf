09816cf233279aa0d6a227b485f5dd907a68a780
Maryam <20474103+MaryamBerg@users.noreply.github.com>
2025-06-23 20:15:30 +0200

    .

--- project/main.tf
+++ project/main.tf
@@ -1,5 +1,6 @@
 resource "google_compute_network" "vpc" {
   name = "webapp-vpc"
+  auto_create_subnetworks=false
 }
 
 resource "google_compute_subnetwork" "subnet" {
@@ -10,31 +11,7 @@ resource "google_compute_subnetwork" "subnet" {
   private_ip_google_access = true
 }
 
-resource "google_compute_firewall" "allow-http" {
-  name    = "allow-http"
-  network = google_compute_network.vpc.name
 
-  allow {
-    protocol = "tcp"
-    ports = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["web"]
-}
-
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "allow-ssh"
-  network = google_compute_network.vpc.name
-
-  allow {
-    protocol = "tcp"
-    ports = ["22"]
-  }
-
-  source_ranges = ["YOUR.IP.ADDRESS/32"] # todo add command to get my ip address
-  target_tags = ["web"]
-}
 
 resource "google_compute_router" "nat_router" {
   name    = "web-nat-router"
@@ -48,6 +25,35 @@ resource "google_compute_router_nat" "nat" {
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+
+#done
+resource "google_compute_firewall" "allow-http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["web"]
+}
+
+#done
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["YOUR.IP.ADDRESS/32"] # todo add command to get my ip address
+  target_tags = ["web"]
 }
 
 #done
